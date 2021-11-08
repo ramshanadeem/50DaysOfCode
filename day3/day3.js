a9a7add9b58508f5cc4cd5069f5be0069a1feb2b
ramshanadeem <rimshanadeem64@gmail.com>
2021-11-08 02:15:47 -0800

    latest

--- day3/day3.js
+++ day3/day3.js
@@ -1,5 +1,4 @@
 let getEvenNumbers = (arr) => {
-  //   let arr = [4, 5, 7, 8, 14, 45, 76];
   for (let i = 0; i < arr.length; i++) {
     if (i % 2 == 0) {
       //   console.log(i);
@@ -10,3 +9,17 @@ let getEvenNumbers = (arr) => {
 };
 
 getEvenNumbers([4, 5, 7, 8, 14, 45, 76]);
+
+const replace = (arr) => {
+  let n = arr.length;
+  let max = arr[n - 1];
+
+  arr[n - 1] = -1;
+  for (let i = n - 2; i >= 0; i--) {
+    let temp = arr[i];
+    arr[i] = max;
+    if (max < temp) max = temp;
+  }
+  return arr;
+};
+console.log(replace([2, 8, 5, 1]));
